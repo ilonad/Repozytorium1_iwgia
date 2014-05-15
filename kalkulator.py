ec4cbe532d31f50b8ea381ea2c38a687e5c2e71c
IlonaDrezek <drezek.ilona@gmail.com>
2014-05-15 17:09:57 +0200

    Dodałam funkcję getInfo()

--- kalkulator.py
+++ kalkulator.py
@@ -2,4 +2,10 @@ def dodawanie():
     a=int(input('Wprowadz 1 liczbę: '))
     b=int(input('Wprowadz 2 liczbę: '))
     print(a+b)
+    
+    
+def getInfo():
+    print("To jest prosty program sumujący liczby")
+
+getInfo()
 dodawanie()
